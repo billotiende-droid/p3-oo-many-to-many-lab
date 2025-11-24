59eacf0a16dedbd741e37629d83f564957ea28c5
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-11-24 23:33:18 +0300

    author-class-contracts-books-royalties

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,15 @@
 class Author:
-    pass
+    def __init__(self, name):
+        self.name = name
 
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
 
-class Book:
-    pass
+    def books(self):
+        return list(set(contract.book for contract in self.contracts()))
 
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
 
-class Contract:
-    pass
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
