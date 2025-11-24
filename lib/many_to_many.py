2b814c22a6d7775b1554bc3f59d4047e0fdab50c
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-11-24 23:36:27 +0300

    contract-class-validate-instances-append-contract

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -25,3 +25,21 @@ class Book:
     def authors(self):
         return list(set(contract.author for contract in self.contracts()))
 
+
+class Contract:
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception("Author must be an instance of Author")
+        if not isinstance(book, Book):
+            raise Exception("Book must be an instance of Book")
+        if not isinstance(date, str):
+            raise Exception("Date must be a string")
+        if not isinstance(royalties, int):
+            raise Exception("Royalties must be an integer")
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
